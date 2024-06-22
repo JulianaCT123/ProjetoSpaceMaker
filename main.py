6042dbc1a4f6e5beb425ced2c9c89916f8efa768
Gabriel Almeida <gquadrosa2@gmail.com>
2024-06-22 14:13:40 -0300

    Sistema de Constelação V.1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from tkinter import simpledialog
+
 
 pygame.init()
 tamanho = (1000,563)
@@ -9,27 +11,36 @@ pygame.display.set_icon(icone)
 pygame.display.set_caption("Space Maker")
 pygame.mixer.music.load("assets/Space_Machine_Power.mp3")
 fundo = pygame.image.load("assets/bg.jpg")
+fonte = pygame.font.SysFont("Arial",15)
 
 branco = (255,255,255)
-
+vermelho = (255,0,0)
+tela.fill(branco)
 
 def jogar():
     posicoes = []
+    nomes = []
+
     while True:
         for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     quit()
-                elif evento.type == pygame.MOUSEBUTTONDOWN:
+                elif evento.type == pygame.MOUSEBUTTONDOWN:                    
                     ponto = (evento.pos)
                     posicoes.append(ponto)
-                    #print(ponto)
-
+                    nome = simpledialog.askstring("...", "Qual o nome dessa estrela ?")
+                    nomes.append(nome)
+                    textoNome = fonte.render(nome,True,vermelho)
+                    tela.blit(textoNome,(ponto))
+                    
         tela.fill(branco)
         tela.blit(fundo,(0,0))
-        
+
         for pos in posicoes:
-            pygame.draw.circle(tela, branco, ponto, 6)
-        
+            pygame.draw.circle(tela, vermelho, pos, 4)
+        for item in range(len(posicoes)-1):
+            pygame.draw.line(tela,vermelho,posicoes[item],posicoes[item + 1], 3)
+
         pygame.display.update()
         clock.tick(60)
 jogar()
