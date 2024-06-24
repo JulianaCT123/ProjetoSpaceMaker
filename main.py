8e1b9d6ba978aacde5061c48165e57f6639771d7
Juliana C. Teixeira <julianact2004@gmail.com>
2024-06-24 15:40:06 -0300

    nomear estrelas, estrelas sem nome mostra a coordenada

--- main.py
+++ main.py
@@ -14,32 +14,31 @@ fundo = pygame.image.load("assets/bg.jpg")
 fonte = pygame.font.SysFont("Arial",15)
 
 branco = (255,255,255)
-vermelho = (255,0,0)
 tela.fill(branco)
 
 def jogar():
     posicoes = []
     nomes = []
-
+    tela.fill(branco)
+    tela.blit(fundo,(0,0))
     while True:
         for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     quit()
-                elif evento.type == pygame.MOUSEBUTTONDOWN:                    
+                elif evento.type == pygame.MOUSEBUTTONDOWN:
                     ponto = (evento.pos)
                     posicoes.append(ponto)
                     nome = simpledialog.askstring("...", "Qual o nome dessa estrela ?")
+                    if not nome:
+                        nome = (f"Desconhecido ({ponto[0]}, {ponto[1]})")
                     nomes.append(nome)
-                    textoNome = fonte.render(nome,True,vermelho)
+                    textoNome = fonte.render(nome,True,branco)
                     tela.blit(textoNome,(ponto))
-                    
-        tela.fill(branco)
-        tela.blit(fundo,(0,0))
 
         for pos in posicoes:
-            pygame.draw.circle(tela, vermelho, pos, 4)
+            pygame.draw.circle(tela, branco, pos, 5)
         for item in range(len(posicoes)-1):
-            pygame.draw.line(tela,vermelho,posicoes[item],posicoes[item + 1], 3)
+            pygame.draw.line(tela,branco,posicoes[item],posicoes[item + 1], 3)
 
         pygame.display.update()
         clock.tick(60)
