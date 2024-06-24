0410b9639b74b5286553c1ebdc73a9339932b35c
Juliana C. Teixeira <julianact2004@gmail.com>
2024-06-24 16:54:59 -0300

    Salvamento das estrelas em um arquivo txt usando dicionario

--- main.py
+++ main.py
@@ -14,31 +14,51 @@ fundo = pygame.image.load("assets/bg.jpg")
 fonte = pygame.font.SysFont("Arial",15)
 
 branco = (255,255,255)
-tela.fill(branco)
+
 
 def jogar():
-    posicoes = []
-    nomes = []
-    tela.fill(branco)
-    tela.blit(fundo,(0,0))
+    
+    estrelas = {}
+
     while True:
         for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
+                    arquivo = open("save.txt","w")
+                    arquivo.write(str(estrelas))
+                    arquivo.close()
+                    quit()
+                elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                    arquivo = open("save.txt","w")
+                    arquivo.write(str(estrelas))
+                    arquivo.close()
                     quit()
                 elif evento.type == pygame.MOUSEBUTTONDOWN:
-                    ponto = (evento.pos)
-                    posicoes.append(ponto)
                     nome = simpledialog.askstring("...", "Qual o nome dessa estrela ?")
                     if not nome:
-                        nome = (f"Desconhecido ({ponto[0]}, {ponto[1]})")
-                    nomes.append(nome)
-                    textoNome = fonte.render(nome,True,branco)
-                    tela.blit(textoNome,(ponto))
+                        nome = (f"Desconhecido {evento.pos}")
+                    estrelas[nome] = evento.pos
+                    print(estrelas)
 
-        for pos in posicoes:
-            pygame.draw.circle(tela, branco, pos, 5)
-        for item in range(len(posicoes)-1):
-            pygame.draw.line(tela,branco,posicoes[item],posicoes[item + 1], 3)
+                elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_F1: # F1 para salvar
+                    try:
+                        arquivo = open("save.txt","w")
+                        arquivo.write(str(estrelas))
+                        arquivo.close()
+                    except:
+                        pass
+        
+        tela.blit(fundo,(0,0))
+        
+        textoSalvar = fonte.render("Aperte F1 para Salvar", True, branco)
+        tela.blit(textoSalvar, (5,0))
+
+        for key, value in estrelas.items():
+                textoNome = fonte.render(key,True,branco)
+                tela.blit(textoNome,value)
+                pygame.draw.circle(tela, branco, value, 5)
+        teste = list(estrelas.values())
+        for pos in range(len(teste) -1):
+                pygame.draw.line(tela,branco, teste[pos], teste[pos +1], 3)
 
         pygame.display.update()
         clock.tick(60)
